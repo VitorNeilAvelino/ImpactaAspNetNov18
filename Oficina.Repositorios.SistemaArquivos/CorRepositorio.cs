35c38bc96a0df68bdea6562d316b60ac42a1d077
Vitor Neil Avelino <avelino.vitor@gmail.com>
2018-11-27 22:27:01 -0200

    MarcaRepositorio

--- Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
+++ Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
@@ -2,19 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Oficina.Repositorios.SistemaArquivos
 {
     public class CorRepositorio
     {
+        const string caminhoArquivo = @"Dados\Cor.txt";
+
         public List<Cor> Selecionar()
         {
             var cores = new List<Cor>();
 
-            foreach (var linha in File.ReadAllLines(@"Dados\Cor.txt"))
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
             {
                 var cor = new Cor();
 
@@ -25,6 +24,31 @@ namespace Oficina.Repositorios.SistemaArquivos
             }
 
             return cores;
+        }
+
+        public Cor Selecionar(int id)
+        {
+            //int? x = null;
+
+            Cor cor = null;
+
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
+            {
+                var linhaId = linha.Substring(0, 5);
+
+                if (Convert.ToInt32(linhaId) == id)
+                {
+                    cor = new Cor();
+
+                    cor.Id = Convert.ToInt32(linha.Substring(0, 5));
+                    cor.Nome = linha.Substring(5);
+                    //null.ToString();
+
+                    break;
+                }
+            }
+
+            return cor;
         }
     }
 }
